62674fae51b23f8669715dd1041e90c99f5e96e6
Shabran29 <160574329+Shabran29@users.noreply.github.com>
2024-05-25 22:31:44 +0700

    Add files via upload

---
+++ Biodata/src/MainProgram/Statement.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MainProgram;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author HP
+ */
+class Statement {
+
+    ResultSet executeQuery(String select__from_mahasiswa) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
